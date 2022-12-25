30b0f9472b24e0062f0e9fbc3d49cebd6b490f34
GitHub Action <action@github.com>
2022-12-25 13:04:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/vlnabatov/alabaster/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vlnabatov/alabaster/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vlnabatov.alabaster.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vlnabatov.alabaster.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/vlnabatov/alabaster/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vlnabatov/alabaster/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vlnabatov.alabaster.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vlnabatov.alabaster.MyBundle
 
 class MyProjectService(project: Project) {
 
